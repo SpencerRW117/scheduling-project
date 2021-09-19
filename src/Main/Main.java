fb43c012cc3b2f5dc9b2fc4dcb3247e7195e5aef
SpencerRW117 <spencerwatkins117@gmail.com>
2021-09-18 23:43:07 -0400

    Credential validation updates

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -7,6 +7,8 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import model.JDBC;
 
+import java.sql.*;
+
 public class Main extends Application {
 
     @Override
@@ -17,9 +19,27 @@ public class Main extends Application {
         primaryStage.show();
     }
 
+    public static void printQuery() throws SQLException{
+        Connection c = JDBC.getConnection();
+        Statement st = c.createStatement();
+        ResultSet rs = st.executeQuery("select * from customers");
 
-    public static void main(String[] args) {
-        //JDBC.makeConnection();
+        ResultSetMetaData metaData = rs.getMetaData();
+        int columnCount = metaData.getColumnCount();
+
+        while(rs.next()) {
+            for(int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+                Object object = rs.getObject(columnIndex);
+                System.out.printf("%s, ", object == null ? "NULL" : object.toString());
+            }
+            System.out.printf("%n");
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        JDBC.makeConnection();
+        //printQuery();
         launch(args);
+
     }
 }
--- src/controller/loginScreenController.java
+++ src/controller/loginScreenController.java
@@ -6,13 +6,19 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import model.JDBC;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 
 /** This class controls loginScreen.fxml @author Spencer Watkins*/
@@ -24,7 +30,43 @@ public class loginScreenController implements Initializable {
 
 
     /** Handles the submit credentials event from the login screen. */
-    public void loginScreenSubmitHandler (ActionEvent actionEvent) throws IOException {
+    public void loginScreenSubmitHandler (ActionEvent actionEvent) throws Exception {
+        boolean validLogin = validateCredentials();
+        if(validLogin){
+            goToMainScreen(actionEvent);
+        }
+
+        else{ return; }
+    }
+
+    /** Validates the credentials against the database. */
+    private boolean validateCredentials() throws SQLException {
+        Connection c = JDBC.getConnection();
+        Statement st = c.createStatement();
+        String userNameAndPassQuery = "SELECT User_Name, Password FROM users";
+        ResultSet rs = st.executeQuery(userNameAndPassQuery);
+        String enteredUserName = loginScreenUsernameField.getText();
+        String enteredUserPassword = loginScreenPasswordField.getText();
+        //System.out.println("STUFF: " + enteredUserPassword + enteredUserName); //Debugging print
+        while(rs.next()) {
+            String currUserName = rs.getString(1);
+            String currPassword = rs.getString(2);
+            //System.out.println("STUFF 2: " + currPassword + currUserName); //Debugging print
+            if (currUserName.equals(enteredUserName) && currPassword.equals(enteredUserPassword)) {
+                return true;
+            }
+        }
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Invalid Credentials");
+        alert.setHeaderText(null);
+        alert.setContentText("Invalid login credentials, please enter a valid username and password.");
+        alert.show();
+        return false;
+    }
+
+    /** Changes the scene to the main dashboard. */
+    private void goToMainScreen(ActionEvent actionEvent) throws IOException{
         Parent root = FXMLLoader.load(getClass().getResource("/view/mainScreen.fxml"));
         Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
         Scene scene = new Scene(root, 906, 537);
@@ -33,10 +75,8 @@ public class loginScreenController implements Initializable {
         stage.show();
     }
 
-
-
     /** Exits the program from the login screen. */
-    public void exitProgram(ActionEvent actionEvent) {
+    public void exitProgram() {
         System.exit(1);
     }
 
